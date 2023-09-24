b5fc8e94ff208d156349f2f8c824f7141860f518
Angelina Ghimire <ghimireangelina123@gmail.com>
2023-09-24 12:16:47 +0545

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Angelina Ghimire",
+  title: "",
+  email: "ghimireangelina123@gmail.com",
+  gitHub: "https://github.com/angelinaghimire",
+  instagram: "https://www.instagram.com/angelinaghimire/",
+  linkedIn: "https://www.linkedin.com/in/angelina-ghimire/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,25 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "/Users/angelinaghimire/Documents/GitHub/technical_onboarding/__images__/bg2.jpegg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "smoky blue abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at KAthmandu University. I enjoy Data Science, Machine Learning and Front End Development.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Science",
+  "Machine Learning",
+  "Deep Learning",
+  "Front End Development"
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about technical communities communities and I am strong advocate for diversity in tech. By leveraging my technical and non technical experiences I continually look for new and better ways to make STEM accessible to all.";
 
 const About = () => {
   return (
